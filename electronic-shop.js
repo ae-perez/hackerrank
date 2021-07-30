8ad6a0b88d197fd8910d7f0285e1084e8af1ac44
Alejandra <perezalejandra.e@gmail.com>
2021-07-30 02:03:06 -0400

    july 30

--- electronic-shop.js
+++ electronic-shop.js
@@ -2,26 +2,21 @@ console.log('testing');
 
 function getMoneySpent(keyboards, drives, b) {
   //create a max variable
-  var max = [];
+  var max = -1; //default value
   var temp = 0; // will temporarily hold the sum to evaluate
 
   for (let i = 0; i < keyboards.length; i++) {
-    //console.log(keyboards[i]);
     for (let j = 0; j < drives.length; j++) {
-      //console.log(drives[j] + keyboards[i]);
       temp = drives[j] + keyboards[i];
-      if (temp <= b) {
-        console.log(temp);
-        max.push(temp);
-      } else {
-        max.push(-1);
+      if (drives[j] + keyboards[i]) {
+        max = Math.max(max, temp);
       }
     }
   }
 
   //console.log(Math.max(...max));
 
-  return Math.max(...max);
+  return max;
 }
 
 var b = 5;
