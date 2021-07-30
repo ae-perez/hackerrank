49285737c282fe7f3658608838b29276c6fa7428
Alejandra <perezalejandra.e@gmail.com>
2021-07-29 20:33:52 -0400

    july 29 done drawing-book final

--- drawing-book.js
+++ drawing-book.js
@@ -6,12 +6,11 @@ p = page the student wants to turn to
 function pageCount(n, p) {
   // Write your code here
 
-  console.log(Math.min(p % 2));
-  return Math.min(Math.trunc(p / 2), Math.trunc(n / 2 - p / 2));
+  return Math.min(Math.trunc(p / 2), Math.trunc(n / 2) - Math.trunc(p / 2));
 }
 
 // how many pages?
-var n = 5;
+var n = 6;
 // what page to turn to?
-var p = 4;
-pageCount(5, 3);
+var p = 5;
+console.log(pageCount(n, p));
