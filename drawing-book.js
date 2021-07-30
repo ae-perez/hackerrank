50e6d603febef32f9fc4995f0304abfe96191799
Alejandra <perezalejandra.e@gmail.com>
2021-07-29 20:24:51 -0400

    july 29 practice drawing-book finished

--- drawing-book.js
+++ drawing-book.js
@@ -5,32 +5,9 @@ p = page the student wants to turn to
 
 function pageCount(n, p) {
   // Write your code here
-  let start = 1;
-  let endStart = n;
-  let turned = 0;
 
-  do {
-    //if the first start + 1 is equal to this.page, than increase turned and
-    if (start++ == p) {
-      turned++;
-      n = p;
-    } else if (start + 2 == p) {
-      turned++;
-      n = p;
-    } else if (endStart % 2 == 0 && endStart-- == p) {
-      turned++;
-      n = p;
-    } else if (endStart % 2 == 0 && endStart - 2 == p) {
-      turned++;
-      n = p;
-    }
-
-    console.log(turned);
-  } while (n != p);
-
-  //console.log(turned);
-
-  return turned;
+  console.log(Math.min(p % 2));
+  return Math.min(Math.trunc(p / 2), Math.trunc(n / 2 - p / 2));
 }
 
 // how many pages?
