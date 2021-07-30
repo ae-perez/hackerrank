8a11a7e380f32ef5e36f60ca511fa47dd1cdc7d8
Alejandra <perezalejandra.e@gmail.com>
2021-07-30 02:07:54 -0400

    july 30 finished practice

--- electronic-shop.js
+++ electronic-shop.js
@@ -1,20 +1,21 @@
 console.log('testing');
 
 function getMoneySpent(keyboards, drives, b) {
-  //create a max variable
-  var max = -1; //default value
+  var max = -1; //default value - return this if not purchasable
   var temp = 0; // will temporarily hold the sum to evaluate
 
+  //nested for loop, to go through both arrays
   for (let i = 0; i < keyboards.length; i++) {
     for (let j = 0; j < drives.length; j++) {
+      //temp will hold the added values to compare
       temp = drives[j] + keyboards[i];
+
+      //if both added together equal to or is less than the budget, figure out the max of those two and set the max value to 'max'
       if (drives[j] + keyboards[i] <= b) {
         max = Math.max(max, temp);
       }
     }
   }
-
-  //console.log(Math.max(...max));
 
   return max;
 }
